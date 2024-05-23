6c80204f66ed959b9d1976d927e30bb03baca968
Priyansh61 <21bec080@iiitdmj.ac.in>
2024-05-23 23:19:58 +0530

    Added util for DB connections

--- CRS-JEDI-FLIPKART-JAVA-POS/src/com/flipkart/utils/DBUtils.java
+++ CRS-JEDI-FLIPKART-JAVA-POS/src/com/flipkart/utils/DBUtils.java
@@ -1,49 +1,72 @@
 package com.flipkart.utils;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
+/**
+ * @author JEDI-Group-C Praneet, Rishabh, Akhil, Manan, Nidhi, Shivanshu, Divyansh
+ * Interface for Admin Dao Operations
+ *
+ */
 public class DBUtils {
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/crs_database";
 
-    public static Connection getConnection() {
-        Connection connection = null;
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "priyanshmehta61";
+    private static Connection connection = null;
+
+    /**
+     * Constructor to return singleton connection
+     * @return Connection
+     */
+    public static Connection getConnection()
+    {
         if (connection != null)
-            return connection;
+        {
+            try
+            {
+                if (connection.isClosed())
+                {
+                    connection = null;
+                    return getConnection();
+                }
+                else
+                {
+                    return connection;
+                }
+            }
+            catch (SQLException e)
+            {
+                return getConnection();
+            }
+        }
         else
         {
             try
             {
-
-                Properties prop = new Properties();
-                InputStream inputStream = DBUtils.class.getClassLoader().getResourceAsStream("./config.properties");
-                prop.load(inputStream);
-                String driver = prop.getProperty("driver");
-                String url = prop.getProperty("url");
-                String user = prop.getProperty("user");
-                String password = prop.getProperty("password");
-                Class.forName(driver);
-                connection = DriverManager.getConnection(url, user, password);
-
+//                Properties prop = new Properties();
+////                InputStream inputStream = DBUtils.class.getClassLoader().getResourceAsStream("./config.properties");
+////                prop.load(inputStream);
+//                String driver = prop.getProperty(JDBC_DRIVER);
+//                String url = prop.getProperty(DB_URL);
+//                String user = prop.getProperty(USER);
+//                String password = prop.getProperty(PASS);
+                Class.forName(JDBC_DRIVER);
+                connection = DriverManager.getConnection(DB_URL, USER, PASS);
+                System.out.println("Connection established");
             }
-            catch (ClassNotFoundException e){
+            catch (ClassNotFoundException e)
+            {
                 e.printStackTrace();
             }
-            catch (SQLException e) {
-                e.printStackTrace();
-            }
-            catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            catch (IOException e) {
+            catch (SQLException e)
+            {
                 e.printStackTrace();
             }
             return connection;
         }
-
     }
 }
